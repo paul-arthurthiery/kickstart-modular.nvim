bb79f21bd7ce0d728743b9ae23a1683e8859edd4
paul-arthurthiery <p.thiery14@gmail.com>
2026-05-25 15:51:38 -0400

    which-key: hide help/keys hints

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -20,6 +20,8 @@ return {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
       delay = 0,
+      show_help = false,
+      show_keys = false,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
