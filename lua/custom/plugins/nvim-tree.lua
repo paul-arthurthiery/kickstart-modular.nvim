446943bf7d655681582beee233fcd7d4dcdb5a35
paul-arthurthiery <p.thiery14@gmail.com>
2026-06-06 18:40:56 -0400

    customize lazygit plugin

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,3 +1,10 @@
+local VIEW_WIDTH_FIXED = 30
+local view_width_max = VIEW_WIDTH_FIXED -- fixed to start
+
+local function get_view_width_max()
+  return view_width_max
+end
+
 return {
   {
     'nvim-tree/nvim-tree.lua',
@@ -9,6 +16,14 @@ return {
     end,
     config = function(_, o)
       require('nvim-tree').setup(o)
+
+      -- Recipes --
+      local api = require 'nvim-tree.api'
+
+      -- Open file on creation
+      api.events.subscribe(api.events.Event.FileCreated, function(file)
+        vim.cmd('edit ' .. vim.fn.fnameescape(file.fname))
+      end)
     end,
     opts = {
       hijack_directories = {
@@ -16,7 +31,10 @@ return {
         auto_open = true,
       },
       view = {
-        width = 35,
+        width = {
+          min = 30,
+          max = get_view_width_max,
+        },
         side = 'left',
       },
       filters = {
@@ -41,12 +59,66 @@ return {
           },
         },
       },
+      live_filter = {
+        always_show_folders = false,
+      },
       on_attach = function(bufnr)
         local api = require 'nvim-tree.api'
         api.config.mappings.default_on_attach(bufnr)
         -- Free up 's' for flash.nvim, remap system-open to 'O'
         vim.keymap.del('n', 's', { buffer = bufnr })
         vim.keymap.set('n', 'O', api.node.run.system, { buffer = bufnr, desc = 'System open' })
+
+        -- h | Collapse current containing folder
+        -- H | Collapse Tree
+        -- l | Open node if it is a folder, else edit the file and close tree
+        -- L | Open node if it is a folder, else create vsplit of file and keep cursor focus on tree
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
+          end
+        end
+
+        -- open as vsplit on current node
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file as vsplit
+            api.node.open.vertical()
+          end
+
+          -- Finally refocus on tree if it was lost
+          api.tree.focus()
+        end
+        vim.keymap.set('n', 'l', edit_or_open, { buffer = bufnr, desc = 'Edit Or Open' })
+        vim.keymap.set('n', 'L', vsplit_preview, { buffer = bufnr, desc = 'Vsplit Preview' })
+        vim.keymap.set('n', 'h', api.tree.close, { buffer = bufnr, desc = 'Close' })
+        vim.keymap.set('n', 'H', api.tree.collapse_all, { buffer = bufnr, desc = 'Collapse All' })
+
+        -- Toggle adaptative width
+        local function toggle_width_adaptive()
+          if view_width_max == -1 then
+            view_width_max = VIEW_WIDTH_FIXED
+          else
+            view_width_max = -1
+          end
+
+          api.tree.reload()
+        end
+
+        vim.keymap.set('n', 'A', toggle_width_adaptive, { buffer = bufnr, desc = 'Toggle Width Adaptive' })
       end,
       -- Sync tree root to git repo root
       sync_root_with_cwd = true,
