7b1dc17bab357e3dd05b4abd5b0465569337cb18
paul-arthurthiery <p.thiery14@gmail.com>
2026-09-01 15:04:43 +0200

    nvim: blink-cmp - enable frecency and prefer rust fuzzy matcher
    Switch fuzzy implementation to prefer_rust_with_warning and enable
    frecency so recently/frequently used completions rank higher.

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -128,7 +128,10 @@ return {
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = {
+        implementation = 'prefer_rust_with_warning',
+        frecency = { enabled = true },
+      },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
